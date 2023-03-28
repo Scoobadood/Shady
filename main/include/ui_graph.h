2d0825d31141960c862e78f1900ba90a3768bbbe
Dave <scoobadood@me.com>
2023-03-28 19:40:29 +1100

    REF: Moved graph rendering into separate UI source files.

---
+++ main/include/ui_graph.h
@@ -0,0 +1,10 @@
+#ifndef IMAGE_TOYS_UI_GRAPH_H
+#define IMAGE_TOYS_UI_GRAPH_H
+
+#include "ui_theme.h"
+/*
+ * Render the graph by rendering the nodes.
+ */
+void render_graph(State &state, const std::shared_ptr<Theme>& theme);
+
+#endif //IMAGE_TOYS_UI_GRAPH_H
