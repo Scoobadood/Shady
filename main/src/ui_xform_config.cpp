6e99ce3fa662c17dd0d14dd67ce8289f63491f6e
Dave <scoobadood@me.com>
2023-03-29 15:04:40 +1100

    Added confi for string and float

--- main/src/ui_xform_config.cpp
+++ main/src/ui_xform_config.cpp
@@ -11,9 +11,14 @@ void maybe_render_config(const std::shared_ptr<const Xform> &xform,
       switch (pd.type) {
         case XformConfig::PropertyDescriptor::FLOAT: {
           float f;
-          ImGui::InputFloat(pd.name.c_str(), &f, 0.01f, 0.1f, "%0.3f");
-          XformConfig c = xform->config();
-          c.set(pd.name, f);
+          xform->config().get(pd.name, f);
+
+          if (ImGui::InputFloat(pd.name.c_str(), &f, .1, 10., "%0.3f", ImGuiInputTextFlags_EnterReturnsTrue)) {
+            const Xform *xfc = xform.get();
+            auto *xf = const_cast<Xform *>(xfc);
+            xf->config().set(pd.name, f);
+            state.graph->evaluate();
+          }
         }
           break;
 
@@ -21,8 +26,8 @@ void maybe_render_config(const std::shared_ptr<const Xform> &xform,
           int i;
           xform->config().get(pd.name, i);
 
-          if( ImGui::InputInt(pd.name.c_str(), &i, 1, 10,  ImGuiInputTextFlags_EnterReturnsTrue ) ) {
-            const Xform * xfc = xform.get();
+          if (ImGui::InputInt(pd.name.c_str(), &i, 1, 10, ImGuiInputTextFlags_EnterReturnsTrue)) {
+            const Xform *xfc = xform.get();
             auto *xf = const_cast<Xform *>(xfc);
             xf->config().set(pd.name, i);
             state.graph->evaluate();
@@ -32,14 +37,19 @@ void maybe_render_config(const std::shared_ptr<const Xform> &xform,
 
 
         case XformConfig::PropertyDescriptor::STRING: {
-          char buf[40];
-          ImGui::InputText(pd.name.c_str(), buf, 39);
-          XformConfig c = xform->config();
-          c.set(pd.name, std::string(buf));
+          std::string s;
+          char buff[40];
+          memset(buff, 0, 40);
+          strncpy(buff, s.c_str(), 39);
+          xform->config().get(pd.name, s);
+          if (ImGui::InputText(pd.name.c_str(), buff, 40, ImGuiInputTextFlags_EnterReturnsTrue)) {
+            const Xform *xfc = xform.get();
+            auto *xf = const_cast<Xform *>(xfc);
+            xf->config().set(pd.name, std::string{buff});
+            state.graph->evaluate();
+          }
         }
           break;
-        default:
-          ImGui::Text("%s", pd.name.c_str());
       }
     }
   }
